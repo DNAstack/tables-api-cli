33e32836464826d99329131715ddcba4bfcdcea6
trandaniel-dna <59574709+trandaniel-dna@users.noreply.github.com>
2020-01-23 11:23:05 -0500

    Tables api cli+version info (#4)
    * Adding version command intitial, disabled breaking tests
    
    * Adding exepction catcher, re-adding tests
    
    * Update version
    
    * Messaging change

--- src/main/java/com/dnastack/ga4gh/tables/cli/cmd/Main.java
+++ src/main/java/com/dnastack/ga4gh/tables/cli/cmd/Main.java
@@ -5,7 +5,10 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.RunLast;
 
-@Command(name = "tables", mixinStandardHelpOptions = true)
+import java.io.IOException;
+import java.util.Properties;
+
+@Command(name = "tables", mixinStandardHelpOptions = true, version = {"tables-api-cli Version : ${tables-api-cli-version}"})
 public class Main extends BaseCmd {
 
     private static CommandLine commandLine = setup();
@@ -14,7 +17,22 @@ public class Main extends BaseCmd {
         return commandLine.execute(args);
     }
 
-    public static void main(String... args) {
+    public static void main(String... args){
+
+        try {
+            final Properties properties = new Properties();
+            properties.load(Main.class.getClassLoader().getResourceAsStream(".properties"));
+            System.setProperty("tables-api-cli-version", properties.getProperty("tables-api-cli-version"));
+        } catch (IOException | NullPointerException | IllegalArgumentException ex) {
+            System.setProperty("tables-api-cli-version", "Unknown value");
+            System.out.println("There was an issue reading the version value. The cause was most likely a\n" +
+                               "compilation error with the .jar file. Please try re-downloading the file\n" +
+                               "from our official github repo : https://github.com/DNAstack/tables-api-cli\n\n" +
+                               "You may continue using this CLI as all other functionality should be \n" +
+                               "unaffected by this error. If you encounter any more issues, please .\n" +
+                               "re-download the file. \n");
+        }
+
         System.exit(runCommand(args));
     }
 
